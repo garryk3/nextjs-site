99daecd8d587ad2a7ec9d0013107b7a13599e7b5
garryk3 <egoruum@yandex.ru>
2019-01-09 01:52:43 +0300

    admin form validate

--- components/admin-resourse/admin-editor.js
+++ components/admin-resourse/admin-editor.js
@@ -2,6 +2,8 @@ import React, { Fragment, PureComponent } from 'react';
 import PropTypes from 'prop-types';
 
 import TextField from '@material-ui/core/TextField';
+import SaveIcon from '@material-ui/icons/Save';
+import Button from '@material-ui/core/Button';
 
 import classnames from 'classnames/bind';
 import style from './style.css';
@@ -17,8 +19,74 @@ class AdminEditor extends PureComponent {
 
     constructor(props) {
         super(props);
+
+        this.defaultState = {
+            isValidForm: false,
+            valueTitle: {
+                value: '',
+                valid: false,
+                focused: false
+            },
+            valueDescription: {
+                value: '',
+                valid: false,
+                focused: false
+            },
+            valueHeading: {
+                value: '',
+                valid: false,
+                focused: false
+            },
+            valueKeywords: {
+                value: '',
+                valid: false,
+                focused: false
+            },
+            valueTextarea: {
+                value: '',
+                valid: false,
+                focused: false
+            }
+        };
+
+        this.state = {
+            ...this.defaultState
+        };
     }
 
+    onClickSaveForm = () => {
+        console.log('save')
+    };
+
+    onChangeField = (name) => {
+        return (event) => {
+            let valid = true;
+            const value = event.target.value;
+
+            switch (name) {
+                // case 'valueTitle': {
+                //     //valid = !!value.length;
+                //     break;
+                // }
+                default: {
+                    valid = !!value.length;
+                }
+            }
+
+            const newState = {
+                [name]: {
+                    ...this.state[name],
+                    value,
+                    valid
+                },
+                isValidForm: this.state.valueTitle.valid && this.state.valueDescription.valid && this.state.valueHeading.valid && this.state.valueKeywords.valid && this.state.valueTextarea.valid
+            };
+
+            !this.state[name].focused && (newState[name].focused = true);
+
+            this.setState(newState)
+        }
+    };
 
     render() {
         return (
@@ -26,6 +94,8 @@ class AdminEditor extends PureComponent {
 
                 <form className={cn('admin-editor__form')}>
                     <TextField
+                        required={true}
+                        error={this.state.valueTitle.focused && !this.state.valueTitle.valid}
                         id="outlined-full-width"
                         label="Title"
                         style={{ margin: '0 0 24px' }}
@@ -37,8 +107,12 @@ class AdminEditor extends PureComponent {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        onChange={this.onChangeField('valueTitle')}
+                        value={this.state.valueTitle.value}
                     />
                     <TextField
+                        required={true}
+                        error={this.state.valueDescription.focused && !this.state.valueDescription.valid}
                         id="outlined-full-width"
                         label="Description"
                         style={{ margin: '0 0 24px' }}
@@ -50,8 +124,12 @@ class AdminEditor extends PureComponent {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        onChange={this.onChangeField('valueDescription')}
+                        value={this.state.valueDescription.value}
                     />
                     <TextField
+                        required={true}
+                        error={this.state.valueHeading.focused && !this.state.valueHeading.valid}
                         id="outlined-full-width"
                         label="Heading"
                         style={{ margin: '0 0 24px' }}
@@ -63,8 +141,12 @@ class AdminEditor extends PureComponent {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        onChange={this.onChangeField('valueHeading')}
+                        value={this.state.valueHeading.value}
                     />
                     <TextField
+                        required={true}
+                        error={this.state.valueKeywords.focused && !this.state.valueKeywords.valid}
                         id="outlined-full-width"
                         label="Keywords"
                         style={{ margin: '0 0 24px' }}
@@ -76,8 +158,12 @@ class AdminEditor extends PureComponent {
                         InputLabelProps={{
                             shrink: true,
                         }}
+                        onChange={this.onChangeField('valueKeywords')}
+                        value={this.state.valueKeywords.value}
                     />
                     <TextField
+                        required={true}
+                        error={this.state.valueTextarea.focused && !this.state.valueTextarea.valid}
                         id="filled-multiline-flexible"
                         label="Содержание статьи"
                         multiline
@@ -85,7 +171,15 @@ class AdminEditor extends PureComponent {
                         fullWidth
                         margin="normal"
                         variant="filled"
+                        onChange={this.onChangeField('valueTextarea')}
+                        value={this.state.valueTextarea.value}
                     />
+                    <div className={cn('admin-editor__save-btn')}>
+                        <Button variant="contained" size="small" disabled={!this.state.isValidForm} onClick={this.onClickSaveForm}>
+                            <SaveIcon />
+                            Save
+                        </Button>
+                    </div>
                 </form>
             </div>
         )
